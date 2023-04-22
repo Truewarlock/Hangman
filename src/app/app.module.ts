f17e491f679af98133f536ac2e95d689e036f829
Truewarlock <valytruewarlock@gmail.com>
2023-04-22 11:56:50 +0300

    directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HangmanDirective } from './directives/hangman.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HangmanDirective
   ],
   imports: [
     BrowserModule
